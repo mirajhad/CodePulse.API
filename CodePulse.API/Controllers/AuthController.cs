b755a1f3de8efaec80667810137497e2bc0a92a9
mirajhad <mirajhadish48@gmail.com>
2024-07-21 16:15:04 +0530

    auth controller added

---
+++ CodePulse.API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            var user = new IdentityUser
+            {
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim(),
+            };
+
+            var identityResult = await _userManager.CreateAsync(user, request.Password);
+            if (identityResult.Succeeded)
+            {
+                identityResult = await _userManager.AddToRoleAsync(user, "Reader");
+
+                if (identityResult.Succeeded)
+                {
+                    return Ok();
+                }
+                if (identityResult.Errors.Any())
+                {
+                    foreach (var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            else
+            {
+                if (identityResult.Errors.Any())
+                {
+                    foreach (var error in identityResult.Errors) 
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return ValidationProblem(ModelState);
+        }
+    }
+}
